f84ecd22c0242b4c8f933411aeaf3f500eb5df8d
аристократ2 <fedyakuklev@mail.ru>
2025-07-09 20:19:12 +0500

    data

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ win.fill((0, 220, 255))
 is_game = True
 
 class GameSprite(pygame.sprite.Sprite):
-   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
        super().__init__()
        self.image = pygame.transform.scale(pygame.image.load(player_image), (wight, height)) #вместе 55,55 - параметры
        self.speed = player_speed
        self.rect = self.image.get_rect()
        self.rect.x = player_x
        self.rect.y = player_y
+    
+    def reset(self):
+        win.blit(self.image, (self.rect.x, self.rect.y))
 
 while is_game:
     for event in pygame.event.get():
